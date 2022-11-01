b197efc80e7ca1afbb6b6e117c772f2be6a710c7
Guilherme Bueno <guilhermebueno6@gmail.com>
2022-11-01 13:17:38 -0400

    Adding Best sellers section

--- resources/views/layouts/bestsellers.blade.php
+++ resources/views/layouts/bestsellers.blade.php
@@ -16,4 +16,65 @@
 
             </div>
         </div>
+        <div class="item">
+            <img src="images/sourdough.png" alt="Churis Bread Products">
+            <div class="bs-body">
+                <div class="top-row">
+                    <h3>Nutella filled donuts</h3>
+                    <h3 class="price">$4.50/ea</h3>
+                </div>
+                <p>Ingredients:<br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/pao_frances.png" alt="Churis Bread Products">
+            <div class="bs-body">
+                <div class="top-row">
+                    <h3>Nutella filled donuts</h3>
+                    <h3 class="price">$4.50/ea</h3>
+                </div>
+                <p>Ingredients:<br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/pao_branco.png" alt="Churis Bread Products">
+            <div class="bs-body">
+                <div class="top-row">
+                    <h3>Nutella filled donuts</h3>
+                    <h3 class="price">$4.50/ea</h3>
+                </div>
+                <p>Ingredients:<br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/pain_au_chocolat.png" alt="Churis Bread Products">
+            <div class="bs-body">
+                <div class="top-row">
+                    <h3>Nutella filled donuts</h3>
+                    <h3 class="price">$4.50/ea</h3>
+                </div>
+                <p>Ingredients:<br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/cupcake.png" alt="Churis Bread Products">
+            <div class="bs-body">
+                <div class="top-row">
+                    <h3>Nutella filled donuts</h3>
+                    <h3 class="price">$4.50/ea</h3>
+                </div>
+                <p>Ingredients:<br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            </div>
+        </div>
+        
     </div>
