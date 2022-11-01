97b869ecb96a2d1d419259fcca94de1a046c2202
AllanDeMauro <allandemauro@gmail.com>
2022-11-01 13:10:33 -0400

    added the membership section
    <b> tag is not working on this font

---
+++ resources/views/layouts/membership.blade.php
@@ -0,0 +1,14 @@
+
+<div class="title-wrapper">
+    <span class="line"></span>
+    <h1 class="title">Membership</h1>
+    <span class="line"></span>
+</div>
+<div class="mycontainer">
+    <div class="membership">
+        <h2><b>Sign up today for our membership program!</b></h2>
+        <h2>What's in for you?<br>Get exclusive offfers, recipes and your order weekly delivered to<br>your door in the comfort of your home! Want to Learn more?</h2>
+        <button>I WANT TO LEARN MORE!</button>
+    </div>
+</div>
+
