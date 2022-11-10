e73bf841e047d71509b0673a426bc7871da63d3f
Guilherme Bueno <guilhermebueno6@gmail.com>
2022-11-09 22:08:32 -0500

    Adding correct images and removing prices on the products section

--- resources/views/layouts/bestsellers.blade.php
+++ resources/views/layouts/bestsellers.blade.php
@@ -5,11 +5,11 @@
     </div>
     <div class="bs-block-wrapper">
         <div class="item">
-            <img src="images/donut.png" alt="Churis Bread Products">
+            <img src="images/products/sourdough.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
                     <h3>Sourdough</h3>
-                    <h3 class="price">$0.90/ea</h3>
+                    <!-- <h3 class="price">$0.90/ea</h3> -->
                 </div>
                 <br>
                 <p>Our levain (Churis) has been maturing and improving in the last 5 years, which transforms completely a simple combination of flour, water and salt. Our sourdough is available in 3 different versions: White, Blended and Multigrains.</p>
@@ -17,11 +17,11 @@
             </div>
         </div>
         <div class="item">
-            <img src="images/sourdough.png" alt="Churis Bread Products">
+            <img src="images/products/white-loaf.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
                     <h3>Loaves</h3>
-                    <h3 class="price">$4.50/ea</h3>
+                    <!-- <h3 class="price">$4.50/ea</h3> -->
                 </div>
                 <br>
                 <p>Our loaves are carefully mixed with no additives or improvers and fermented in order to offer a perfect softness and texture to upgrade your sanduiches. There are 3 different versions available: White, Wholewheat and Multigrains.</p>
@@ -29,11 +29,11 @@
             </div>
         </div>
         <div class="item">
-            <img src="images/pao_frances.png" alt="Churis Bread Products">
+            <img src="images/products/croissant.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
                     <h3>Croissant</h3>
-                    <h3 class="price">$4.50/ea</h3>
+                    <!-- <h3 class="price">$4.50/ea</h3> -->
                 </div>
                 <br>
                 <p>Our 24 hours fermented laminated dough made from scratch with real butter, which provides several layers of butter and crispy golden brown dough, available in 4 different flavours: Plain, Ham and Cheese, Spinach and Feta, and pain au chocolat (chocolate).</p>
@@ -41,11 +41,11 @@
             </div>
         </div>
         <div class="item">
-            <img src="images/pao_branco.png" alt="Churis Bread Products">
+            <img src="images/products/baguette.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
                     <h3>Baguette</h3>
-                    <h3 class="price">$4.50/ea</h3>
+                    <!-- <h3 class="price">$4.50/ea</h3> -->
                 </div>
                 <br>
                 <p>High hydration dough adding to a polish starter, and olive oil, to promote the crunchiness and flavour typical of a real fresh baguette. Our baguettes are an upgrade for your sandwiches.</p>
@@ -53,11 +53,11 @@
             </div>
         </div>
         <div class="item">
-            <img src="images/pain_au_chocolat.png" alt="Churis Bread Products">
+            <img src="images/products/blueberry-scone.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
                     <h3>Scones</h3>
-                    <h3 class="price">$4.50/ea</h3>
+                    <!-- <h3 class="price">$4.50/ea</h3> -->
                 </div>
                 <br>
                 <p>Our scones are prepared from scratch. No pre-mixes are added. They are a good option combined with coffee and tea. They are available in 6 different flavours: Lemon blueberry, white chocolate raspberry, strawberry with pineapple topping, Apple caramel, Cinamon, and triple chocolate.</p>
@@ -65,11 +65,11 @@
             </div>
         </div>
         <div class="item">
-            <img src="images/cupcake.png" alt="Churis Bread Products">
+            <img src="images/products/cinnamon-roll.jpeg" alt="Churis Bread Products">
             <div class="bs-body">
                 <div class="top-row">
-                    <h3>Cinnamon Buns</h3>
-                    <h3 class="price">$4.50/ea</h3>
+                    <h3>Cinnamon Rolls</h3>
+                    <!-- <h3 class="price">$4.50/ea</h3> -->
                 </div>
                 <br>
                 <p>Soft dough filled with butter, brown sugar and cinnamon, and topped with our in house cream cheese topping. Available in 2 different versions: Big (individual) and Mini (pack of 6).</p>
