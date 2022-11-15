05de81e2bd294af72b38890cdf42f6b85c40ace9
Guilherme Bueno <guilhermebueno6@gmail.com>
2022-11-15 11:29:10 -0500

    Adding location and business hours

--- resources/views/layouts/location.blade.php
+++ resources/views/layouts/location.blade.php
@@ -3,8 +3,29 @@
         <h1 class="title">Location</h1>
         <span class="line"></span>
     </div>
-<h2 class="mb-5">We are located at 1040 Adelaide St N, London, ON N5Y 2M9</h2>
-<h2 class="mb-5">Call us on: (226) 317-4130</h2>
+    <div class="location-business-wrapper">
+
+        <div class="location">
+            <h2 class="mb-5">We are located at 1040 Adelaide St N, London, ON N5Y 2M9</h2>
+            <h2 class="mb-5">Call us on: (548) 689-9675</h2>
+        </div>
+        <div class="business-hours mb-5">
+            <h2 class="mb-5">Business hours:</h2>
+            <ul>
+                <li>Monday: 8a.m.-6p.m.</li>
+                <li>Tuesday:  8a.m.-6p.m.</li>
+                <li>Wednesday: 8a.m.-6p.m.</li>
+                <li>Thursday: 8a.m.-6p.m.</li>
+                <li>Friday: 8a.m.-6p.m.</li>
+                <li>Saturday: 9a.m.-6p.m.</li>
+                <li>Sunday: 9a.m.-4p.m.</li>
+            </ul>
+        </div>
+
+    </div>
+    
+
+
 <div style="height: 450px;" class="map mb-5">
 	{!! Mapper::render() !!}
 </div>
