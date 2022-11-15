cc7f69faf34e21b87bf442ccee56c45ba79c35b4
Guilherme Bueno <guilhermebueno6@gmail.com>
2022-11-14 22:17:42 -0500

    Adding phone number on the location section

--- resources/views/layouts/location.blade.php
+++ resources/views/layouts/location.blade.php
@@ -4,6 +4,7 @@
         <span class="line"></span>
     </div>
 <h2 class="mb-5">We are located at 1040 Adelaide St N, London, ON N5Y 2M9</h2>
-<div style="height: 450px;" class="map">
+<h2 class="mb-5">Call us on: (226) 317-4130</h2>
+<div style="height: 450px;" class="map mb-5">
 	{!! Mapper::render() !!}
 </div>
